bec010df476413cfe5bd38f25b8ce14bcd2736dc
TofDowe <68544755+TofDoweHub@users.noreply.github.com>
2023-03-08 17:05:05 +0500

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number;
+			int triesCount = 5;
+			int numberUserInput;
+			Random random = new Random();
+
+			Console.WriteLine("Игра ,,Отгадай число``.");
+			Console.WriteLine("Введите число от 1 до 10 включительно:");
+			number = random.Next(1, 10);
+
+			while (triesCount-- > 0)
+			{
+				numberUserInput = Convert.ToInt32(Console.ReadLine());
+				if (numberUserInput == number)
+				{
+					Console.WriteLine("Поздравляем! Вы выиграли.");
+					Console.ReadLine();
+					break;
+				}
+				else if (numberUserInput != number)
+				{
+					Console.WriteLine("Вы ввели не то число! попробуйте ещё раз.");
+					Console.WriteLine($"У вас осталось попыток: {triesCount}");
+				}
+
+				else
+				{
+					Console.WriteLine($"Увы, вы проиграли. Число было равно {number}.");
+				}
+			}
+		}
+	}
+}
